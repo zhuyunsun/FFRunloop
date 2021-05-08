9b5ce7fd1892f07d7fad4e761fd5ae9a0e57b457
zhuyun <1935589833@qq.com>
2021-05-08 18:01:45 +0800

    Update ViewController.m

--- FFRunloop/ViewController.m
+++ FFRunloop/ViewController.m
@@ -15,16 +15,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     
     
+#pragma mark runloop的概念
+    /*
+     一个线程执行一次任务,执行完任务之后会退出.
+     */
+    
+#pragma mark runloop和线程的关系
     
     
+#pragma mark runloop相关的类
     
     
+#pragma mark runloop中的mode
     
     
-    
+#pragma mark runloop的应用
     
     
 }
